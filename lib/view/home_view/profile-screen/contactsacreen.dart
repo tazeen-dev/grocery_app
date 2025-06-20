7b311e0718fdf43e193b09e700d91c311f50924f
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-06-20 05:53:11 -0700

    adding multiple screen in profile folder

---
+++ lib/view/home_view/profile-screen/contactsacreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class ContactScreen extends StatelessWidget {
+  const ContactScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('hello'),
+    );
+  }
+}
--- lib/view/home_view/profile-screen/profile-screen.dart
+++ lib/view/home_view/profile-screen/profile-screen.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/controller/components/grey-text.dart';
+import 'package:grocery_app/controller/components/profile_listveiw.dart';
 import 'package:grocery_app/controller/utils/constants/appcolors/app_color.dart';
+import 'package:grocery_app/controller/utils/constants/appicons/app_icons.dart';
+import 'package:grocery_app/controller/utils/constants/appimages/app_images_widget.dart';
+import 'package:grocery_app/model/product_model/profile_list_model.dart';
+import 'package:grocery_app/view/home_view/profile-screen/aboutscreen.dart';
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+   ProfileScreen({super.key});
+  List<ProfileList> items=[
+    ProfileList(AboutScreen(), text: 'About me', image: AppImages.profile, icon:Icons.arrow_forward_ios,),
+    ProfileList(, text: text, image: image, icon: icon)
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -33,9 +43,11 @@ class ProfileScreen extends StatelessWidget {
                         width: 100,
                         decoration: BoxDecoration(
                           shape: BoxShape.circle,
-                          image: DecorationImage(image: )
+                          image: DecorationImage(image: AssetImage(AppImages.profile),)
                         ),
-                      )
+                      ),
+                      GreyText(text: 'oliviaaustin@gmail.com'),
+                      ListClass(profileitems: ),
                     ],
                   ),),
               )
