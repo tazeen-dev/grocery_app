3190e7ceda89087a8312d429d4ced5bec80b0f02
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-06-20 19:05:50 -0700

    successfully build about me screen

--- lib/view/home_view/profile-screen/aboutscreen.dart
+++ lib/view/home_view/profile-screen/aboutscreen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/controller/components/green_text_button.dart';
 import 'package:grocery_app/controller/components/grey-text.dart';
+import 'package:grocery_app/controller/components/password-feild.dart';
 import 'package:grocery_app/controller/components/text-class.dart';
+import 'package:grocery_app/controller/components/textfield.dart';
 import 'package:grocery_app/controller/utils/constants/appcolors/app_color.dart';
 import 'package:grocery_app/controller/utils/constants/appicons/app_icons.dart';
 class AboutScreen extends StatefulWidget {
@@ -11,67 +14,81 @@ class AboutScreen extends StatefulWidget {
 }
 
 class _AboutScreenState extends State<AboutScreen> {
+  TextEditingController passwordController=TextEditingController();
+  TextEditingController passwordConfirmController=TextEditingController();
+  TextEditingController checkPasswordController=TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: AppColors.whiteColor,
         title: BlackTextWidget(text: 'About me',fontSize: 
         18,fontWeight: FontWeight.w500,),
         centerTitle: true,
-        leading: Image(image: AssetImage(AppIcons.backicon)),
+        leading: Image(image: AssetImage(AppIcons.backicon,),color: AppColors.blackColor,),
       ),
-      body: Column(
-        children: [
-          BlackTextWidget(text:'Personal Details',
-            fontWeight: FontWeight.w600,fontSize: 18, ),
-          SizedBox(height: 20,),
-          Container(
-            height: 60,
-            width: 350,
-            decoration: BoxDecoration(
-              color: AppColors.whiteColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 25,),
+            BlackTextWidget(text:'Personal Details',
+              fontWeight: FontWeight.w600,fontSize: 18, ),
+            SizedBox(height: 20,),
+            Container(
+              height: 60,
+              width: 350,
+              decoration: BoxDecoration(
+                color: AppColors.whiteColor,
+              ),
+              child: Row(
+                children: [
+                  Image(image: AssetImage(AppIcons.person)),
+                  SizedBox(width: 20,),
+                  GreyText(text: 'Russell Austin') ,             ],
+              ),
             ),
-            child: Row(
-              children: [
-                Image(image: AssetImage(AppIcons.person)),
-                SizedBox(width: 20,),
-                GreyText(text: 'Russell Austin') ,             ],
+            SizedBox(height: 15,),
+            Container(
+              height: 60,
+              width: 350,
+              decoration: BoxDecoration(
+                color: AppColors.whiteColor,
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.email_outlined,color: AppColors.greyColor,),
+                  SizedBox(width: 20,),
+                  GreyText(text: 'russell.partner@gmail.com') ,             ],
+              ),
             ),
-          ),
-          SizedBox(height: 15,),
-          Container(
-            height: 60,
-            width: 350,
-            decoration: BoxDecoration(
-              color: AppColors.whiteColor,
+            SizedBox(height: 15,),
+            Container(
+              height: 60,
+              width: 350,
+              decoration: BoxDecoration(
+                color: AppColors.whiteColor,
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.phone,color: AppColors.greyColor,)
+                 , SizedBox(width: 20,),
+                  GreyText(text: '+1  202  555  0142 ') ,             ],
+              ),
             ),
-            child: Row(
-              children: [
-                Icon(Icons.email_outlined,color: AppColors.greyColor,),
-                SizedBox(width: 20,),
-                GreyText(text: 'russell.partner@gmail.com') ,             ],
-            ),
-          ),
-          SizedBox(height: 15,),
-          Container(
-            height: 60,
-            width: 350,
-            decoration: BoxDecoration(
-              color: AppColors.whiteColor,
-            ),
-            child: Row(
-              children: [
-                Icon(Icons.phone,color: AppColors.greyColor,)
-               , SizedBox(width: 20,),
-                GreyText(text: '+1  202  555  0142 ') ,             ],
-            ),
-          ),
-          SizedBox(height: 30,),
-          BlackTextWidget(text:
-          'Change Password',fontSize:18,fontWeight:
-          FontWeight.w600,),
-          T
-        ],
+            SizedBox(height: 30,),
+            BlackTextWidget(text:
+            'Change Password',fontSize:18,fontWeight:
+            FontWeight.w600,),
+            SizedBox(height: 20,),
+            TextFeildWidget(hintext: 'Current password', prefixIcons: Icons.lock_outline, controller: passwordController),
+            PasswordWidget(hintext: 'password', prefixIcons: Icons.lock_outline, controller: checkPasswordController),
+            TextFeildWidget(hintext: 'Confirm password', prefixIcons: Icons.lock_outline, controller: passwordConfirmController),
+           SizedBox(height: 40,),
+            GreenTextButton(text: 'Save settings', ontap: (){})
+          ],
+        ),
       ),
     );
   }
--- lib/view/home_view/profile-screen/orderscreen.dart
+++ lib/view/home_view/profile-screen/orderscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/ma''terial.dart';
+import 'package:grocery_app/controller/components/text-class.dart';
 class OrderScreen extends StatefulWidget {
   const OrderScreen({super.key});
 
@@ -10,7 +11,15 @@ class _OrderScreenState extends State<OrderScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('hello'),
+      appBar: AppBar(
+        title: BlackTextWidget(text: 'My Order',fontWeight: FontWeight.w600,fontSize: 18,),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          
+        ],
+      ),
     );
   }
 }
