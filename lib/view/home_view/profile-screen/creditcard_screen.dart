129c1e52b8b4480e2d739f32e4800cef9ae4716e
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-06-22 03:20:55 -0700

    build credit screen

--- lib/view/home_view/profile-screen/creditcard_screen.dart
+++ lib/view/home_view/profile-screen/creditcard_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/controller/components/text-class.dart';
+import 'package:grocery_app/controller/utils/constants/appcolors/app_color.dart';
+import 'package:grocery_app/controller/utils/constants/appicons/app_icons.dart';
 class CreditcardScreen extends StatefulWidget {
   const CreditcardScreen({super.key});
 
@@ -10,6 +13,11 @@ class _CreditcardScreenState extends State<CreditcardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.whiteColor,
+        title: BlackTextWidget(text: 'Add To card'),
+        leading: Image(image: AssetImage(AppIcons.backicon)),
+      ),
       body: Text('heelo'),
     );
   }
