aeb9849251fd66d0055379b2daa1f7e873e6c106
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-06-23 23:28:39 -0700

    successfully build verify screen
    I used  column inside column I used DropDown button which is wra by row and textform feild which is wrap by expanded .....

--- lib/view/auth_view/varification_screen/verify_number.dart
+++ lib/view/auth_view/varification_screen/verify_number.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/controller/components/green_text_button.dart';
+import 'package:grocery_app/controller/components/grey-text.dart';
+import 'package:grocery_app/controller/components/text-class.dart';
+import 'package:grocery_app/controller/utils/constants/appcolors/app_color.dart';
+
+import '../../../controller/utils/constants/appicons/app_icons.dart';
 class VeryfyNumber extends StatefulWidget {
   @override
   _VeryfyNumberState createState() => _VeryfyNumberState();
@@ -11,60 +17,94 @@ class _VeryfyNumberState extends State<VeryfyNumber> {
   {'name': 'USA', 'flag': '🇺🇸', 'code': '+1'},
   {'name': 'UK', 'flag': '🇬🇧', 'code': '+44'},
 ];
- late Map<String,String> selectedCountry;
-  void initSate(){
+  late Map<String, String> selectedCountry;
+  @override
+  void initState() {
     super.initState();
-    selectedCountry==countries[0];
+    selectedCountry = countries[2];
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Column(
-        children: [
-          SizedBox(height: 108,),
-          Row(
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: DropdownButtonHideUnderline(
-            child: DropdownButton<Map<String, String>>(
-            value: selectedCountry,
-            items: countries.map((country) {
-              return DropdownMenuItem<Map<String, String>>(
-                value: country,
-                child: Row(
-                  children: [
-                    Text(country['flag']!),
-                    SizedBox(width: 5),
-                    Text(country['code']!),
-                  ],
-                ),
-              );
-            }).toList(),
-            onChanged: (value) {
-              setState(() {
-                selectedCountry = value!;
-              });
+      appBar: AppBar(
+        backgroundColor: AppColors.whiteColor,
+        title: BlackTextWidget(text: 'Verify Number',fontWeight: FontWeight.w500,fontSize: 18,),
+        centerTitle: true,
+        leading: InkWell(
+            onTap: (){
+              Navigator.pop(context);
             },
-            ),
-            ),),
-            SizedBox(width: 10),
-            Expanded(
-              child: TextField(
-                controller: phoneController,
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                  hintText: 'Enter phone number',
-                  border: OutlineInputBorder(),
+            child: Image(image: AssetImage(AppIcons.backicon),color: AppColors.blackColor,)),
+      ),
+      body:SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 95,),
+            BlackTextWidget(text: 'Verify your number',fontWeight: FontWeight.w600,fontSize: 25,),
+            SizedBox(height: 10,),
+            GreyText(text: 'Lorem ipsum dolor sit amet, consetetur \n sadipscing elitr, sed diam nonumy'),
+           SizedBox(height: 40,),
+            Row(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                decoration: BoxDecoration(
+                  color: AppColors.whiteColor,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: DropdownButtonHideUnderline(
+              child: DropdownButton<Map<String, String>>(
+              value: selectedCountry,
+              items: countries.map((country) {
+                return DropdownMenuItem<Map<String, String>>(
+                  value: country,
+                  child: Row(
+                    children: [
+                      Text(country['flag']??'',),
+                      SizedBox(width: 5),
+                      Text(country['code']??'',),
+                    ],
+                  ),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedCountry = value!;
+                });
+              },
+              ),
+              ),),
+              SizedBox(width: 5),
+              Expanded(
+                child: TextField(
+                  controller: phoneController,
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    hintText: '2055550145',
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(color: AppColors.whiteColor,)
+                    ),
+                    filled: true,
+                    fillColor: AppColors.whiteColor,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: AppColors.whiteColor),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  style: TextStyle(color: AppColors.blackColor),
                 ),
               ),
+            ],),
+            SizedBox(
+              height: 30,
             ),
-          ],),
-        ],
+            GreenTextButton(text: 'Next', ontap: (){}),
+            SizedBox(height: 10,),
+            BlackTextWidget(text: 'Resend confirmation code (1:23)',fontSize: 15,fontWeight: FontWeight.w400,)
+          ],
+        ),
       ),
     );
   }
