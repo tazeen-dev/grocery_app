09fb098c9402128444737a4261f71b95149b6d77
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-06-29 03:17:59 -0700

    ....

--- lib/view/home_view/apply_filters/apply_filters.dart
+++ lib/view/home_view/apply_filters/apply_filters.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/controller/components/green_text_button.dart';
 import 'package:grocery_app/controller/components/textfield.dart';
 import 'package:grocery_app/model/product_model/textfeild.dart';
 
@@ -31,127 +32,146 @@ class _ApplyFiltersState extends State<ApplyFilters> {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 20,),
- Container(
-   height: 449,
-   width:double.infinity,
-   decoration: BoxDecoration(
-     color: AppColors.whiteColor,
-   ),
-   child: Column(
-     children: [
-       BlackTextWidget(text: 'Price Range',fontSize: 15,fontWeight: FontWeight.w600,),
-       Row(
-         children: [
-           Expanded(
-             child: TextFormField(
-               controller: minController,
-               decoration: InputDecoration(
-                 enabledBorder: OutlineInputBorder(
-                   borderSide: BorderSide(color: AppColors.lightGrey),
-                   borderRadius: BorderRadius.circular(5),
-                 ),
-                 focusedBorder: OutlineInputBorder(
-                   borderRadius: BorderRadius.circular(5),
-                   borderSide: BorderSide(color: AppColors.lightGrey)
-                 ),
-                 hintText: 'min',
-                 hintStyle: TextStyle(color: AppColors.greyColor),
-                 filled: true,
-                 fillColor: AppColors.lightGrey
-               ),
-               style: TextStyle(color: AppColors.greyColor),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 20,),
+            Container(
+             height: 449,
+             width:double.infinity,
+             decoration: BoxDecoration(
+         color: AppColors.whiteColor,
              ),
-           ),
-           SizedBox(width: 10,),
-           Expanded(
-             child: TextFormField(
-               controller: minController,
-               decoration: InputDecoration(
-                   enabledBorder: OutlineInputBorder(
-                     borderSide: BorderSide(color: AppColors.lightGrey),
-                     borderRadius: BorderRadius.circular(5),
-                   ),
-                   focusedBorder: OutlineInputBorder(
-                       borderRadius: BorderRadius.circular(5),
-                       borderSide: BorderSide(color: AppColors.lightGrey)
-                   ),
-                   hintText: 'Max.',
-                   hintStyle: TextStyle(color: AppColors.greyColor),
-                   filled: true,
-                   fillColor: AppColors.lightGrey
-               ),
-               style: TextStyle(color: AppColors.greyColor),
-             ),
-           ),
-         ],
-       ),
-       Divider(thickness: 1,color: AppColors.greyColor,),
-       BlackTextWidget(text: 'Star Rating',fontSize: 15,fontWeight: FontWeight.w600,),
-       Container(
-         height: 45,
-         width: 349,
-         decoration: BoxDecoration(
-           color: AppColors.lightGrey
-         ),
-         child: Row(
+             child: Padding(
+         padding: const EdgeInsets.symmetric(horizontal: 8.0),
+         child: Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
            children: [
-             Icon(Icons.star,color: Color(0xffFFC107),size:20 ,),
-             Icon(Icons.star,color: Color(0xffFFC107),size:20,),
-             Icon(Icons.star,color: Color(0xffFFC107),size:20 ,),
-             Icon(Icons.star,color: Color(0xffFFC107),size:20 ,),
-             Icon(Icons.star,color: Colors.white,size:15 ,),
-             Text('4stars',style: TextStyle(color: AppColors.greyColor),)
+             SizedBox(height: 30,),
+             BlackTextWidget(text: 'Price Range',fontSize: 15,fontWeight: FontWeight.w600,),
+             SizedBox(height: 10,),
+             Row(
+               children: [
+                 Expanded(
+                   child: TextFormField(
+                     controller: minController,
+                     decoration: InputDecoration(
+                       enabledBorder: OutlineInputBorder(
+                         borderSide: BorderSide(color: AppColors.lightGrey),
+                         borderRadius: BorderRadius.circular(5),
+                       ),
+                       focusedBorder: OutlineInputBorder(
+                         borderRadius: BorderRadius.circular(5),
+                         borderSide: BorderSide(color: AppColors.lightGrey)
+                       ),
+                       hintText: 'min',
+                       hintStyle: TextStyle(color: AppColors.greyColor),
+                       filled: true,
+                       fillColor: AppColors.lightGrey
+                     ),
+                     style: TextStyle(color: AppColors.greyColor),
+                   ),
+                 ),
+                 SizedBox(width: 10,),
+                 Expanded(
+                   child: TextFormField(
+                     controller: minController,
+                     decoration: InputDecoration(
+                         enabledBorder: OutlineInputBorder(
+                           borderSide: BorderSide(color: AppColors.lightGrey),
+                           borderRadius: BorderRadius.circular(5),
+                         ),
+                         focusedBorder: OutlineInputBorder(
+                             borderRadius: BorderRadius.circular(5),
+                             borderSide: BorderSide(color: AppColors.lightGrey)
+                         ),
+                         hintText: 'Max.',
+                         hintStyle: TextStyle(color: AppColors.greyColor),
+                         filled: true,
+                         fillColor: AppColors.lightGrey
+                     ),
+                     style: TextStyle(color: AppColors.greyColor),
+                   ),
+                 ),
+               ],
+             ),
+             SizedBox(height: 30,),
+             Divider(thickness: 1,color: AppColors.greyColor,),
+             SizedBox(height: 30,),
+             BlackTextWidget(text: 'Star Rating',fontSize: 15,fontWeight: FontWeight.w600,),
+             SizedBox(height: 10,),
+             Container(
+               height: 45,
+               width: 349,
+               decoration: BoxDecoration(
+                 color: AppColors.lightGrey
+               ),
+               child: Row(
+                 children: [
+                   Icon(Icons.star,color: Color(0xffFFC107),size:20 ,),
+                   Icon(Icons.star,color: Color(0xffFFC107),size:20,),
+                   Icon(Icons.star,color: Color(0xffFFC107),size:20 ,),
+                   Icon(Icons.star,color: Color(0xffFFC107),size:20 ,),
+                   Icon(Icons.star,color: Colors.white,size:15 ,),
+                   Text('4stars',style: TextStyle(color: AppColors.greyColor),)
+                 ],
+               ),
+             ),
+             SizedBox(height: 30,),
+            Divider(thickness: 1,color: AppColors.greyColor,),
+             SizedBox(height: 30,),
+             BlackTextWidget(text: 'Star Rating',fontSize: 15,fontWeight: FontWeight.w600,),
+             SizedBox(height: 30,),
+             Row(
+             children: [
+               SizedBox(width: 10,),
+              Icon(Icons.discount_outlined,color: AppColors.greyColor,),
+                SizedBox(width: 10,),
+                BlackTextWidget(text: 'Discount',fontSize: 12,fontWeight: FontWeight.w500,textColor:
+                AppColors.greyColor,),
+               SizedBox(width: 270,),
+               Icon(Icons.check_circle_outline_outlined,color: AppColors.greyColor),
+        
+             ],
+           ),
+             SizedBox(width: 20,),
+             Divider(thickness: 1,color: AppColors.greyColor,),
+             SizedBox(height: 30,),
+             Row(
+               children: [
+                 SizedBox(width: 10,),
+                 Image(image: AssetImage(AppIcons.outofdeleverd)),
+                 SizedBox(width: 10,),
+                 BlackTextWidget(text: 'Free shipping',fontSize: 12,fontWeight: FontWeight.w500,textColor:
+                 AppColors.greyColor,),
+                 SizedBox(width: 220,),
+                 Icon(Icons.check_circle_outline_outlined,color: AppColors.DarkGreen),
+        
+               ],
+             ),
+             SizedBox(height: 30,),
+             Divider(thickness: 1,color: AppColors.greyColor,),
+             SizedBox(height: 30,),
+             Row(
+               children: [
+                 SizedBox(width: 10,),
+                 Image(image: AssetImage(AppIcons.order),color: AppColors.greyColor,),
+                 SizedBox(width: 10,),
+                 BlackTextWidget(text: 'Same day delivery',fontSize: 12,fontWeight: FontWeight.w500,textColor:
+                 AppColors.greyColor,),
+                 SizedBox(width: 215,),
+                 Icon(Icons.check_circle_outline_outlined,color: AppColors.DarkGreen),
+               ],
+             ),
            ],
          ),
-       ),
- Divider(thickness: 1,color: AppColors.greyColor,),
-       BlackTextWidget(text: 'Star Rating',fontSize: 15,fontWeight: FontWeight.w600,),
-       Row(
-       children: [
-         SizedBox(width: 10,),
-        Icon(Icons.discount_outlined,color: AppColors.greyColor,),
-          SizedBox(width: 10,),
-          BlackTextWidget(text: 'Discount',fontSize: 12,fontWeight: FontWeight.w500,textColor:
-          AppColors.greyColor,),
-         SizedBox(width: 270,),
-         Icon(Icons.check_circle_outline_outlined,color: AppColors.greyColor),
-
-       ],
-     ),
-       SizedBox(width: 20,),
-       Divider(thickness: 1,color: AppColors.greyColor,),
-       Row(
-         children: [
-           SizedBox(width: 10,),
-           Image(image: AssetImage(AppIcons.outofdeleverd)),
-           SizedBox(width: 10,),
-           BlackTextWidget(text: 'Free shipping',fontSize: 12,fontWeight: FontWeight.w500,textColor:
-           AppColors.greyColor,),
-           SizedBox(width: 220,),
-           Icon(Icons.check_circle_outline_outlined,color: AppColors.DarkGreen),
-
-         ],
-       ),
-       Divider(thickness: 1,color: AppColors.greyColor,),
-       Row(
-         children: [
-           SizedBox(width: 10,),
-           Image(image: AssetImage(AppIcons.order),color: AppColors.greyColor,),
-           SizedBox(width: 10,),
-           BlackTextWidget(text: 'Same day delivery',fontSize: 12,fontWeight: FontWeight.w500,textColor:
-           AppColors.greyColor,),
-           SizedBox(width: 215,),
-           Icon(Icons.check_circle_outline_outlined,color: AppColors.DarkGreen),
-         ],
-       )
-     ],
-   ),
- )
-        ],
+             ),
+           ),
+            SizedBox(height: 100,),
+            GreenTextButton(text: 'Apply filter', ontap: (){}),
+          ],
+        ),
       ),
     );
   }
